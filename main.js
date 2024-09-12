72dd53677db0d7291cde83f685c44631b41d1e10
lmassa.fms <lmassa.fms@gmail.com>
2024-09-12 16:48:05 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// console.log('this line of code is no longer needed'); 
+
+/* Reto 1:
+Crea un arreglo que represente una lista de personas y muestra este arreglo en la consola. */
+let arreglo = ["Fran", "San", 34];
+console.log(arreglo);
+/* Reto 2:
+Usa los operadores aritméticos para realizar las siguientes operaciones: suma, res- ta, multiplicación y división entre 10 y 2. Imprime los resultados de las operaciones en la consola. */
+let num1 = 10;
+let num2 = 2;
+let suma = num1 + num2;
+let resta = num1 - num2;
+let multiplicacion = num1 * num2;
+let division = num1 / num2;
+console.log(suma, resta, multiplicacion, division);
+
